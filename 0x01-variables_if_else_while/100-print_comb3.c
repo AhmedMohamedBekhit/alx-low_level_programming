cc2f4eb39775b098b3f33854c44bb24c0c570bbc
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-20 23:10:17 +0200

    Nice code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - assign a random number to int n
+ * each time it executes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i = 0;
+int j = 1;
+while (i < 98)
+{
+	if (i != j)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+	}
+	j++;
+	if (i != 98)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
