1930739ac2f715cc87407ab2efc11d6703e87089
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-20 21:28:05 +0200

    Nice code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - assign a random number to int n
+ * each time it executes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+	if (c != 'q' && c != 'e')
+	{
+		putchar(c);
+		c++;
+	}
+}
+putchar('\n');
+return (0);
+}
