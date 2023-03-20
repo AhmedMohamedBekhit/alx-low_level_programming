26e9dc9bca8e5e68cbd82bd867720b50876cd6bb
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-20 22:02:30 +0200

    Nice code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - assign a random number to int n
+ * each time it executes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char c = 'z';
+while (c <= 'a')
+{
+	putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
