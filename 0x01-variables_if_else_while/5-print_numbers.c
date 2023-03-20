2679df7b7a924bea1e373fe5e255967811e0edc1
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-20 21:36:19 +0200

    Nice code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - assign a random number to int n
+ * each time it executes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+for (int i = 0; i < 10; i++ )
+{
+	printf("%i", i);
+}
+return (0);
+}
