0f5d4012426e4b67434896723fc354b774a5db10
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-20 22:51:39 +0200

    Nice code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - assign a random number to int n
+ * each time it executes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+	putchar(i);
+	putchar(',');
+	putchar('\t');
+	i++;
+}
+putchar('\n');
+return (0);
+}
