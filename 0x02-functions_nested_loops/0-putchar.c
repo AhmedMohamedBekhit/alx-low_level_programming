b83f0557ce5bcb66fe545b05b71a5f5b7770b14d
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-22 23:29:54 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-# include <stdlib.h>
-# include <stdio.h>
+# include "main.h"
 /*
 * print 
 * return 0
 *
 */
 
-int prints(void)
+int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return(0);
 }
