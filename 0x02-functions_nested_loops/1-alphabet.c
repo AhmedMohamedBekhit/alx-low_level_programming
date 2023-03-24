4bb26055440739cf21e54f23773d175d8794f541
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 13:35:28 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,5 +8,11 @@
 int main(void)
 {
 	print_alphabet();
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,2 @@
 char _putchar (char c);
-void print_alphabet(void)
-{
-	char c = 'a';
-	while (c <= 'z')
-	{
-		_putchar(c);
-	}
-	_putchar('\n');	
-}
+void print_alphabet(void);
