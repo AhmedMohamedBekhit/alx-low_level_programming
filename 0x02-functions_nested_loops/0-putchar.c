14901311057772fa44b531996786c5814980f4a8
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 13:06:00 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 # include "main.h"
 /*
-* print 
-* return (0)
-*
+*print 
+*return (0)
 */
 
 int main(void)
@@ -16,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
