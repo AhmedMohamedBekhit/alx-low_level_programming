09d8cb8e9dfc2b3dd135150beb5adb11170e62c6
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 23:41:35 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			if ( n == 98)
 			{
-				printf("%i \n", n);
+				printf("%i\n", n);
 			}
 			else
 			{
@@ -28,12 +28,19 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
+			if ( n == 98)
+			{
+				printf("%i\n", n);				
+			}
+			else
+			{
 			printf("%i, ", n);
+			}
 			n--;
 		}
 	}
 	else
 	{
-		printf("%i", n);
+		printf("%i\n", n);
 	}
 }
