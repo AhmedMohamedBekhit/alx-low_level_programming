07fffa041ed68f27feb86509b5c9c77d13455335
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 17:56:28 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - display time 
+ * jack_bauer - display time
  * @i: increase to 2
  * @j: increase to 3
  * @k: increase to 5
@@ -14,12 +14,15 @@ void jack_bauer(void)
 	while (i <= 2)
 	{
 		int j = 0;
+		
 		while (j <= 3)
 		{
 			int k = 0;
+			
 			while (k <= 5)
 			{
 				int l = 0;
+				
 				while (l <= 9)
 				{
 					_putchar(i + '0');
