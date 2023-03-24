abf9559297c6ddf73159e62bb072c9133b8a1040
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 13:30:29 +0200

    Ramadan Kareem

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main - Entry point
+*include main function
+*Return: nothing | return void
+*prints _putchar
+*return (0)
+*/
+
+int main(void)
+{
+	/* import main.h */
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,10 @@
 char _putchar (char c);
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');	
+}
