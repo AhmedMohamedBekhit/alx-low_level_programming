63b610d41369a98dc41e5fc81d97fd7d7cb0d425
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 15:24:01 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * _abs - abselute value of int
- * @i:
- * Return:
+ * @i: if i less than 0 print change the abs
+ * Return: return i
  */
- int _abs(int i)
- {
- 	if (i < 0)
+
+int _abs(int i)
+{
+ 	if(i < 0)
  	{
- 		i = i * -1 ;
+ 		i = i * -1;
  	}
  	return (i);
- }
+}
