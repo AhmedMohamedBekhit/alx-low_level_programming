6604e9d920440fd2d5efd8761b8c58cf4a6680dc
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 16:35:07 +0200

    Ramadan Kareem

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit by modules
+ * @i: i equal i mod 10
+ * Return: return i
+ */
+
+int print_last_digit(int i)
+{
+	i = i % 10;
+	_putchar(i);
+	return (i);
+} 
