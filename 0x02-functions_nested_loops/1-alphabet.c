1520093e8250448688c54747726f2b651f29b445
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-24 13:50:40 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,12 @@
  * main - check the code for Holberton School students.
  * print_alphabet - Not Entry point - function to print from a to z
  * Return: Always 0.
+ * print_alphabet - function
  */
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
