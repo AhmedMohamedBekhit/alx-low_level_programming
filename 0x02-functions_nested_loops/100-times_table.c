b848bb74ca7a3d6820e5d5c8b500ce110e4f484a
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-28 16:46:21 +0200

    Ramadan Kareem

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * @i: to increase 
+ * print_times_table - print time table 
+ * @n: to increase n 
+ **/
+void print_times_table(int n)
+{
+	if (!((n < 0) || (n > 15)))
+	{
+		int i = 0;
+		while (i < n)
+		{
+			_putchar(i);
+			
+		}
+	}
+}
