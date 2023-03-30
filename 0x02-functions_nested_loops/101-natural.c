66bd09b0c8c9ac24e01b9b8e723ef73c439cf9c6
AhmedMohamedBekhit <ahmedmohamedbekhit@gmail.com>
2023-03-31 00:32:00 +0200

    Ramadan Kareem

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,14 +3,15 @@
 /**
  * main - print the sum of digit devid 3 or 5
  * Return: always (0)
- * @i: increase value
- * @sum: sum
+ * @i: increase value less than 1024
+ * @sum: addition operation
  **/
 
-int main (void)
+int main(void)
 {
 	int i = 0;
 	int sum = 0;
+
 	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
